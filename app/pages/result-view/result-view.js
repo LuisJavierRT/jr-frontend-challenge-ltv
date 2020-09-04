e5e5ad2996ac0275e1da16a7c36b11d17078ed33
Luis Javier Ramirez Torres <luisjavier@Luiss-MacBook-Pro.local>
2020-09-03 20:30:59 -0600

    -Added search input validations -Fixed search input and search button css -Added person information data on the result view

--- app/pages/result-view/result-view.js
+++ app/pages/result-view/result-view.js
@@ -1,6 +1,32 @@
 "use strict"
 
-var getPersonData = function() {
+function search() {
+    var floatingLabel = document.getElementById('floating-label');
+    var emailElement = document.getElementById('email');
+    var email = emailElement.value;
+    if (email === '') {
+        return;
+    }
+
+    if (!validateEmail(email)) {
+        console.log('Invalid Email');
+        floatingLabel.style.color = 'red';
+        floatingLabel.innerHTML = 'Please provide a valid email';
+        emailElement.className = 'search-input-error';
+        return;
+    }
+
+    floatingLabel.style.color = 'gray';
+    emailElement.className = 'search-input';
+    window.location.href = '../result-view/result-view.html?email=' + email;
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function getPersonData() {
     var BASE_URL = 'https://ltv-data-api.herokuapp.com/api/v1/';
     
     var queryString = window.location.search;
@@ -17,11 +43,38 @@ var getPersonData = function() {
         
         if (Http.responseText === '[]') {
             console.log('Could not find email');
+            alert('Could not find data for that email. Please try again using another email');
+            window.location.href = '../search-view/search-view.html';
             return;
         }
 
-        console.log('DATA', Http.responseText);
+        setPersonData(JSON.parse(Http.responseText));
     }  
+}
+
+function setPersonData(personData) {
+    document.getElementById('name').innerHTML = `${personData.first_name} ${personData.last_name}`;
+    document.getElementById('description').innerHTML = personData.description;
+    document.getElementById('address').innerHTML = personData.address;
+    document.getElementById('person-email').innerHTML = personData.email; 
+    
+    var ul = document.getElementById("phone-numbers");
+    ul.innerHTML = '';
+    for( var i = 0; i < personData.phone_numbers.length; i++ ) { 
+        var obj = personData.phone_numbers[i];
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(obj));
+        ul.appendChild(li);     
+    }
+
+    var ul = document.getElementById("relatives");
+    ul.innerHTML = '';
+    for( var i = 0; i < personData.relatives.length; i++ ) { 
+        var obj = personData.relatives[i];
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(obj));
+        ul.appendChild(li);     
+    }
 }
 
 getPersonData();
--- app/pages/search-view/search-view.js
+++ app/pages/search-view/search-view.js
@@ -1,12 +1,27 @@
 "use strict"
 
 function search() {
-    var BASE_URL = 'https://ltv-data-api.herokuapp.com/api/v1/';
-    var email = document.getElementById('email').value;
+    var floatingLabel = document.getElementById('floating-label');
+    var emailElement = document.getElementById('email');
+    var email = emailElement.value;
     if (email === '') {
-        // Error no email
         return;
     }
 
+    if (!validateEmail(email)) {
+        console.log('Invalid Email');
+        floatingLabel.style.color = 'red';
+        floatingLabel.innerHTML = 'Please provide a valid email';
+        emailElement.className = 'search-input-error';
+        return;
+    }
+
+    floatingLabel.style.color = 'gray';
+    emailElement.className = 'search-input';
     window.location.href = '../result-view/result-view.html?email=' + email;
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
